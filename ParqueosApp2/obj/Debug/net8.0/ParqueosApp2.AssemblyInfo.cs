f9f421511a80f1bf8332e2004a3b71fa4bd5cddd
jmunozc023 <jmunozc023@ulacit.ed.cr>
2025-02-28 02:38:57 -0600

    cambio de idioma

--- ParqueosApp2/obj/Debug/net8.0/ParqueosApp2.AssemblyInfo.cs
+++ ParqueosApp2/obj/Debug/net8.0/ParqueosApp2.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ParqueosApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b775b625c1d27f6b390e510e5707f53f6ed62ecd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ParqueosApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ParqueosApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
